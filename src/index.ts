995c9e7a269fc759d96539083c6cb40efce19207
Aniol Rodriguez <arodrigue96@gmail.com>
2024-09-11 21:13:11 +0200

    Added  exercise 3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-/*// Ejercicio 1
+// Ejercicio 1
 
 let years: number[] = [];
 let firstYear = 2000;
@@ -57,8 +57,37 @@ const words = [
 words.reverse();
 let joinedArray = words.join(" ");
 console.log(joinedArray);
-*/
+
 // Ejercicio 3:
 
-let examMarks: number[] = [];
-examMarks = [8, 6, 9, 4, 7];
+let studentexamMarks: number[] = [];
+studentexamMarks = [8, 6, 9, 4, 7];
+let sortedExamMarks = studentexamMarks.sort();
+
+for (const examMark of studentexamMarks) {
+  if (examMark >= 5) {
+    console.log(`Este alumno ha aprobado el examen con un: ${examMark}`);
+  }
+}
+
+let averageStudentMarks = 0;
+
+for (
+  let arrayPosition = 0;
+  arrayPosition < studentexamMarks.length;
+  arrayPosition++
+) {
+  averageStudentMarks += studentexamMarks[arrayPosition];
+}
+
+console.log(
+  `La nota media de los ${studentexamMarks.length} alumnos ha sido: un ${
+    averageStudentMarks / studentexamMarks.length
+  }`
+);
+
+console.log(
+  `La nota más baja ha sido un ${
+    sortedExamMarks[0]
+  } y a nota más alta ha sido un ${sortedExamMarks.at(-1)}`
+);
