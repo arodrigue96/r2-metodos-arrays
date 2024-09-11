24514e033ab1717f30ee124f775b33f4ea6e8bec
Aniol Rodriguez <arodrigue96@gmail.com>
2024-09-11 20:40:14 +0200

    Upload exercises 1 and 2

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+/*// Ejercicio 1
+
+let years: number[] = [];
+let firstYear = 2000;
+let lastYear = 2024;
+
+for (let year = firstYear; year <= lastYear; year++) {
+  years.push(year);
+  firstYear++;
+}
+
+console.log(years);
+
+// Ejercicio 2
+
+const words = [
+  "confín.",
+  "otro",
+  "al",
+  "uno",
+  "del",
+  "conocido",
+  "mar",
+  "todo",
+  "en",
+  "Temido,",
+  "el",
+  "bravura",
+  "su",
+  "por",
+  "llaman,",
+  "le",
+  "pirata",
+  "Bajel",
+  "bergantín.",
+  "velero",
+  "un",
+  "vuela",
+  "sino",
+  "mar",
+  "el",
+  "corta",
+  "no",
+  "vela,",
+  "toda",
+  "a",
+  "popa",
+  "en",
+  "viento",
+  "banda,",
+  "por",
+  "cañones",
+  "diez",
+  "Con",
+];
+
+words.reverse();
+let joinedArray = words.join(" ");
+console.log(joinedArray);
+*/
+// Ejercicio 3:
+
+let examMarks: number[] = [];
+examMarks = [8, 6, 9, 4, 7];
